1ad7fd158ade1d03c5870c88a76d90a3570548d2
Leandro Maia <leandroc@inatel.br>
2021-10-29 21:20:20 -0300

    Add filter to admin.py

--- api_app/admin.py
+++ api_app/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Person
 
-admin.site.register(Person)
+
+class PersonDetailAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "email")
+
+
+
+admin.site.register(Person,PersonDetailAdmin)
